5b06551e68efefb484a65ea0538f1ef25366528c
Sebastian Ernest Jarmul <sebastianernestjarmul@MacBook-Pro-de-user.local>
2023-08-28 17:08:48 +0200

    Move db factory to separate file

--- server/database/index.js
+++ server/database/index.js
@@ -1,35 +1,3 @@
-const mongo = require("./mongo");
-const { DatabaseInterface } = require("../interfaces");
-
-class Database extends DatabaseInterface {
-  constructor(chats, users, messages, connect) {
-    super();
-    this.chats = chats;
-    this.users = users;
-    this.messages = messages;
-    this.connect = connect;
-  }
-}
-
-class DatabaseFactory {
-  static create(type) {
-    switch (type) {
-      case "mongodb":
-        return new Database(
-          new mongo.ChatTable(),
-          new mongo.UserTable(),
-          new mongo.MessageTable(),
-          mongo.MongoConnector.connect
-        );
-      default:
-        return new Database(
-          new mongo.ChatTable(),
-          new mongo.UserTable(),
-          new mongo.MessageTable(),
-          mongo.MongoConnector.connect
-        );
-    }
-  }
-}
+const DatabaseFactory = require("./database.factory");
 
 module.exports = DatabaseFactory.create(process.env.DATABASE);
